83dcd54e565205b5e55bbaf344b2d1c53cf4ea69
galvinma <mattgalvin47@gmail.com>
2018-03-27 15:51:29 -0700

    archetyping clustering implementation

--- algorithm/hierdenc.go
+++ algorithm/hierdenc.go
@@ -6,19 +6,45 @@ package hierdenc
 //     "os"
 //     "log"
 //     "path/filepath"
-//
-//     "gonum.org/v1/gonum/mat"
 // )
 
+func FindClusters(objects map[int][]int, r int) {
+    // Initialize variables
+    r := 1            // radius of hypercubes
+    k := 0            // number of leaf clusters
+    kr := 0           // number of clusters at level r
+    Gk := []int       // kth cluster
+    U := [][]int        // set of hypercube centers
 
-// Find the densest object in dataset.
+    // Get densest hypercube and HIERDENC density index
+    densest, index := hierdenc.HierdencIndex(objects, r)
+    n := len(index)           // number of objects
+    // should come up with a better way of grabbing number of attributes. This is
+    // unnecessary and will increase runtime.
+    m := len(index[0])        // number of catagorical attributes
+
+    for r < m {
+        // If density is 1, increase r
+        if index[densest] <= 1 {
+            r = r + 1
+        }
+        // Merge clusters connected via distance r...
+              // code here...
+
+        // Find a new leaf cluster
+        k = k + 1           // expand the number of leaf clusters by 1
+        Gk = append(Gk, )
 
 
 
-// Initial implementation is a simple search.
-// Later create an index function that supplies constantly refreshed list.
 
-    // Verify densest cube > 1 object. If not, expand radius.
+    }
+
+
+}
+
+
+
 
 // Create a new leaf cluster.
 
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-    "log"
+    // "log"
 
     "github.com/galvinma/agora/algorithm"
 )
@@ -9,10 +9,6 @@ import (
 func main() {
 
   soybeans := hierdenc.InitSoybeanLarge()
+  clusters := hierdenc.FindClusters(soybeans)
 
-  densest, index := hierdenc.HierdencIndex(soybeans, 1)
-  for key, value := range index {
-      log.Println("Key:", key, "Value:", value)
-  }
-  log.Println(densest)
 }
