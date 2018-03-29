510067c50a6751483c49e1f274e9125a8421eba5
galvinma <mattgalvin47@gmail.com>
2018-03-29 08:04:20 -0700

    basic hierdenc

--- algorithm/hierdenc.go
+++ algorithm/hierdenc.go
@@ -38,9 +38,23 @@ func HIERDENC(objects map[int][]int) map[int]int{
             // } else {
             // Cluster ID in the map
 
-            // If object not in map, create a new cluster
+            // If object not in map, check if it can be added to an existing clusters
+            if clusters[object.ID] == 0 {
+              // for obj #, cluster #
+              for k,v := range clusters {
+                  hd, _ := HammingDistance(objects[object.ID], objects[k])
+                  if hd <= r {
+                      // Add object to found cluster
+                      clusters[object.ID] = v
+                      break
+                  }
+              }
+            }
+
+            // If object cannot be added to an existing cluster, create a new cluster
             if clusters[object.ID] == 0 {
                 clusters[object.ID] = id
+                // for obj #, attributes
                 for k,v := range objects {
                     hd, _ := HammingDistance(objects[object.ID], v)
                     if hd <= r {
@@ -48,9 +62,9 @@ func HIERDENC(objects map[int][]int) map[int]int{
                         clusters[k] = id
                     }
                 }
+                // Increment the counter
                 id++
             }
-
 
             // }
         }
