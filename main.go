3eca0d68283f5fc1e0ace712c767c90b32224ba8
galvinma <mattgalvin47@gmail.com>
2018-04-06 14:53:27 -0700

    first pass at sub-functions for modularity calculation

--- main.go
+++ main.go
@@ -12,8 +12,14 @@ func main() {
   data := common.InitSoybeanLarge()
   network := louvain.InitialCommunities(data)
   weights := louvain.GetWeights(data)
-  // for k,v := range(network) {
-  //     log.Println("For object",k,"the assigned cluster is",v)
-  // }
+
+  // test
+  sumin := louvain.SumCommunityWeights(data, network, weights, 1)
+  sumtot := louvain.SumIncidentCommunityWeights(data, network, weights, 1)
+  ki := louvain.GetIncidentWeights(data, weights, 1)
+  kiin := louvain.GetIncidentCommunityWeights(network, weights, 1, 1)
+  m := louvain.SumNetworkLinks(weights)
+
+  log.Println("sumin:", sumin, "sumtot:", sumtot, "ki:", ki, "kiin:", kiin, "m:", m)
 
 }
