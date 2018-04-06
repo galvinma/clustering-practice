2000b05a585e00f2a7e0bd9ede0bfacbb376335a
galvinma <mattgalvin47@gmail.com>
2018-04-06 09:19:03 -0700

    Objects are now contained within initial communities.

--- algorithm/louvain/utility.go
+++ algorithm/louvain/utility.go
@@ -1,11 +1,9 @@
 package louvain
 
 import (
-    // "log"
+    "log"
 
 )
-
-
 
 // Parse objects in map for a given community and return a member list
 func GetMembers(network map[int]int, community int) []int {
@@ -18,16 +16,15 @@ func GetMembers(network map[int]int, community int) []int {
     }
     return members
 }
-//
-// // Assign each node to its own community.
-// func InitialCommunities(data map[int][]int) map[int]int {
-//     // Each object is assigned to its own community.
-//     log.Println("Assigning objects to initial communities..")
-//     var assigned map[int]int
-//     assigned = make(map[int]int)
-//     length := len(data)
-//     for k,_ := range(data) {
-//         assigned[k] = k
-//     }
-//     return assigned
-// }
+
+// Assign each node to its own community.
+func InitialCommunities(data map[int][]int) map[int]int {
+    // Each object is assigned to its own community.
+    log.Println("Assigning objects to initial communities..")
+    var assigned map[int]int
+    assigned = make(map[int]int)
+    for k,_ := range(data) {
+        assigned[k] = k
+    }
+    return assigned
+}
--- main.go
+++ main.go
@@ -10,10 +10,10 @@ import (
 func main() {
   log.Println("Initializing data...")
   data := common.InitSoybeanLarge()
-  weighted := louvain.GetWeights(data)
-  // network := louvain.InitialCommunities(data)
-  // for k,v := range(weighted) {
-  //     log.Println("For object",k,"the weight vector is",v)
-  // }
+  // weighted := louvain.GetWeights(data)
+  network := louvain.InitialCommunities(data)
+  for k,v := range(network) {
+      log.Println("For object",k,"the assigned cluster is",v)
+  }
 
 }
